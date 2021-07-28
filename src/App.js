42ffc4a2bc49799a8318a2c1c27d177e0edaea0b
Chiew Carol <carol.chiew@remarkgroup.com>
2021-07-28 16:02:06 +0800

    Add rendering of shiba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useState } from "react";
+import Shiba from "./shiba";
 
 function App() {
+  const [direction, setDirection] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div
+        style={{
+          width: "100%",
+          height: 30,
+          position: "absolute",
+          top: 10,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          fontSize: 10,
+          zIndex: 10,
+        }}
+      >
+        Implemented by "<a href="http://www.kyaroru.com">kyaroru</a>" ~ Rendered
+        using GLTF format using "
+        <a href="https://github.com/pmndrs/react-three-fiber">
+          react-three-fiber
         </a>
-      </header>
+        " + "<a href="https://github.com/pmndrs/gltfjsx">gltfjsx</a>"
+      </div>
+      <Canvas style={{ width: "100%", height: "100%" }}>
+        <Suspense fallback={null}>
+          <Shiba scale={2} direction={direction} />
+        </Suspense>
+      </Canvas>
+      <div
+        onClick={() => setDirection(!direction)}
+        style={{
+          width: "100%",
+          height: 60,
+          position: "absolute",
+          bottom: 30,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            borderRadius: 10,
+            backgroundColor: "#1dcaf5",
+            width: 200,
+            textAlign: "center",
+            height: 40,
+            color: "#fff",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "grab",
+          }}
+        >
+          Disturb Shiba
+        </div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: 30,
+          position: "absolute",
+          bottom: 0,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          fontSize: 10,
+        }}
+      >
+        This Shiba model is based on "
+        <a href="https://sketchfab.com/3d-models/shiba-faef9fe5ace445e7b2989d1c1ece361c">
+          Shiba
+        </a>
+        " by "<a href="https://sketchfab.com/dogerlo">zixisun02</a>" licensed
+        under "
+        <a href="http://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a>"
+      </div>
     </div>
   );
 }
